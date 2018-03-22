086851113047e3368b2abbee928d8ea0d4cd774c
Caleb Peterson <caleb.m.peterson@gmail.com>
2018-03-22 00:01:39 -0500

    Update env.js

--- src/env.js
+++ src/env.js
@@ -13,5 +13,7 @@ module.exports = {
   getApplicationVersion,
   SETUP_BROWSER_URL: 'https://www.howtogeek.com/114176/HOW-TO-EASILY-CREATE-SEARCH-PLUGINS-ADD-ANY-SEARCH-ENGINE-TO-YOUR-BROWSER/',
   SETUP_CHROME_URL: 'https://support.google.com/chrome/answer/95426?hl=en',
+  SETUP_FIREFOX_URL: 'https://support.mozilla.org/en-US/kb/add-or-remove-search-engine-firefox#w_add-a-search-engine',
+  SETUP_SAFARI_URL: 'https://support.apple.com/guide/safari/customize-your-search-ibrwe75c2a3c/mac',
   SETUP_EDGE_URL: 'https://support.microsoft.com/en-us/help/4028574/windows-10-change-the-default-search-engine-in-microsoft-edge'
 };
