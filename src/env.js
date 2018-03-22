5342f139416fcd6c485db96edad13052af02cdc7
Caleb Peterson <caleb.m.peterson@gmail.com>
2018-03-21 21:05:35 -0500

    Update env.js

--- src/env.js
+++ src/env.js
@@ -11,5 +11,7 @@ function getApplicationVersion() {
 module.exports = {
   getApplicationName,
   getApplicationVersion,
-  SETUP_BROWSER_URL: 'https://www.howtogeek.com/114176/HOW-TO-EASILY-CREATE-SEARCH-PLUGINS-ADD-ANY-SEARCH-ENGINE-TO-YOUR-BROWSER/'
+  SETUP_BROWSER_URL: 'https://www.howtogeek.com/114176/HOW-TO-EASILY-CREATE-SEARCH-PLUGINS-ADD-ANY-SEARCH-ENGINE-TO-YOUR-BROWSER/',
+  SETUP_CHROME_URL: 'https://support.google.com/chrome/answer/95426?hl=en',
+  SETUP_EDGE_URL: 'https://support.microsoft.com/en-us/help/4028574/windows-10-change-the-default-search-engine-in-microsoft-edge'
 };
