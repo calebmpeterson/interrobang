c4d3ed4183a25fdc79279db0f61d5707d44446a8
Caleb Peterson <caleb.peterson@cubicle6.com>
2018-05-16 07:09:00 -0500

    removed failing vestigual test

--- src/config.js
+++ src/config.js
@@ -2,20 +2,4 @@ const { assign, flatten, get, zipObject, isEmpty } = require('lodash');
 
 const github = require('./config/github');
 
-function fromPayload(payload) {
-  const searchEngine = get(payload, 'search-engine');
-  const searchEngineConfig = searchEngine
-        ? { 'search-engine': searchEngine }
-        : {};
-
-  const { bang, pattern } = payload;
-
-  const bangs = zipObject(flatten([bang]), flatten([pattern]));
-  const bangsConfig = !isEmpty(bangs)
-        ? { bangs }
-        : {};
-
-  return assign({}, searchEngineConfig, bangsConfig);
-}
-
-module.exports = assign({}, github, { fromPayload });
+module.exports = github;
