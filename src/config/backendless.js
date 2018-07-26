251219a41d62a9803a20504a37f5d4d984c29f1c
Caleb Peterson <caleb.peterson@cubicle6.com>
2018-07-26 06:56:35 -0500

    enable !! and !!config for backendless configs

--- src/config/backendless.js
+++ src/config/backendless.js
@@ -1,5 +1,5 @@
 const Wreck = require('wreck');
-const { get, isString, isObject } = require('lodash');
+const { get, isString, isObject, merge } = require('lodash');
 
 const APP_ID = process.env.APP_ID;
 const REST_KEY = process.env.REST_KEY;
@@ -11,12 +11,22 @@ function getConfigFileUrl(userId) {
   return `https://api.backendless.com/${APP_ID}/${REST_KEY}/files/configurations/${userId}.config.json`;
 }
 
+function createExtendedConfig(userId) {
+  return {
+    'bangs': {
+      '!': `/b/${userId}`,
+      '!config': `/account/#/configuration`
+    }
+  };
+}
+
 async function getConfig(userId) {
   const url = getConfigFileUrl(userId);
   console.log(`Get config: ${url}`);
 
   const { payload } = await Wreck.get(url);
-  return JSON.parse(payload.toString());
+  const config = JSON.parse(payload.toString());
+  return merge({}, config, createExtendedConfig(userId));
 }
 
 module.exports = {
