19de13673109c471b626e593a74e1db41b1c5fb8
Caleb Peterson <caleb.peterson@cubicle6.com>
2018-10-11 07:41:53 -0400

    simplify footer

--- src/views/components/Footer.jsx
+++ src/views/components/Footer.jsx
@@ -1,36 +1,57 @@
-const React = require('react');
+const React = require("react");
 
-const { getApplicationName, getApplicationVersion } = require('../../env');
+const { getApplicationName, getApplicationVersion } = require("../../env");
 
-const delim = (<span> &bull; </span>);
+const delim = <span> &bull; </span>;
 
 const nameAndVersion = `${getApplicationName()} ${getApplicationVersion()}`;
 
+const contact = "caleb.peterson@cubicle6.com";
+
 const DefaultFooterContent = () => (
   <span>
-    <a href="/" data-toggle="tooltip" data-placement="top" title={nameAndVersion}>Interrobang</a>
+    <a
+      href="/"
+      data-toggle="tooltip"
+      data-placement="top"
+      title={nameAndVersion}
+    >
+      Interrobang
+    </a>
     {delim}
-    Made by Caleb Peterson with <i className="mdi mdi-nodejs text-success"></i> and <i className="mdi mdi-react text-success"></i> in Texas
+    Made by <a href={`mailto:${contact}`}>Caleb Peterson</a> with{" "}
+    <i className="mdi mdi-nodejs text-success" /> and{" "}
+    <i className="mdi mdi-react text-success" />
     <span className="d-none d-md-inline">{delim}</span>
     <br className="d-md-none" />
-    <a href="https://www.github.com/calebmpeterson/interrobang" target="_blank">Source on GitHub</a>
+    <a href="https://www.github.com/calebmpeterson/interrobang" target="_blank">
+      Source on GitHub
+    </a>
     {delim}
-    <a href="http://www.uptimedoctor.com/publicreport/25lngu4k/156281" target="_blank">Status</a>
-    {delim}
-    <img src="https://app.codeship.com/projects/84685410-0b95-0136-a02e-3a0ed76fbcd7/status?branch=master" />
+    <a
+      href="http://www.uptimedoctor.com/publicreport/25lngu4k/156281"
+      target="_blank"
+    >
+      Status
+    </a>
   </span>
 );
 
 class Footer extends React.Component {
   render() {
     const { children, fixedBottom, className } = this.props;
-    const fixedBottomClassName = fixedBottom ? 'fixed-bottom' : '';
+    const fixedBottomClassName = fixedBottom ? "fixed-bottom" : "";
     return (
-      <div className={`footer text-center p-2 ${className || ''} ${fixedBottomClassName}`}>
-        <small>
-          {children || (<DefaultFooterContent />)}
-        </small>
-        <script type="text/javascript" defer src="/assets/js/enable-tooltips.js" />
+      <div
+        className={`footer text-center p-2 ${className ||
+          ""} ${fixedBottomClassName}`}
+      >
+        <small>{children || <DefaultFooterContent />}</small>
+        <script
+          type="text/javascript"
+          defer
+          src="/assets/js/enable-tooltips.js"
+        />
       </div>
     );
   }
