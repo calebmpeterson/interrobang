e13a4c23dd3a77d13915e807d8bea811ae1559f8
Caleb Peterson <caleb.peterson@cubicle6.com>
2018-10-11 07:41:05 -0400

    correct navbar layout on mobile

--- src/views/components/Navbar.jsx
+++ src/views/components/Navbar.jsx
@@ -1,45 +1,66 @@
-const React = require('react');
+const React = require("react");
 
-const Icon = require('./Icon');
-const If = require('./If');
+const Icon = require("./Icon");
+const If = require("./If");
 
 class Navbar extends React.Component {
   render() {
     const { variant, canSignup, canLogin, canSetup, canConfigure } = this.props;
-    const navbarVariantClassName = variant === 'light' ? 'navbar-dark' : 'navbar-light';
+    const navbarVariantClassName =
+      variant === "light" ? "navbar-dark" : "navbar-light";
 
-    const primaryVariantButtonClassName = variant === 'light' ? 'btn-light' : 'btn-primary';
-    const secondaryVariantButtonClassName = variant === 'light' ? 'btn-outline-light' : 'btn-outline-primary';
+    const primaryVariantButtonClassName =
+      variant === "light" ? "btn-light" : "btn-primary";
+    const secondaryVariantButtonClassName =
+      variant === "light" ? "btn-outline-light" : "btn-outline-primary";
 
     return (
-      <nav className={`navbar navbar-expand-sm fixed-top ${navbarVariantClassName}`}>
+      <nav
+        className={`navbar navbar-expand-sm fixed-top ${navbarVariantClassName}`}
+      >
         <div className="container">
-          <a className="navbar-brand mr-auto" href="/">Interrobang</a>
+          <a className="navbar-brand mr-auto" href="/">
+            Interrobang
+          </a>
 
           <If test={canLogin}>
-            <a className={`btn ${secondaryVariantButtonClassName}`} href="/account/#/login">
+            <a
+              className={`btn ${secondaryVariantButtonClassName}`}
+              href="/account/#/login"
+            >
               Login
             </a>
           </If>
           <If test={canSignup}>
-            <a className={`btn ${primaryVariantButtonClassName} ml-2`} href="/account/#/register">
+            <a
+              className={`btn ${primaryVariantButtonClassName} ml-2`}
+              href="/account/#/register"
+            >
               Sign Up
             </a>
           </If>
 
           <If test={canSetup}>
-            <a className={`btn btn-link mr-2`} href="/account/#/configuration/browser">
-              Setup Your Browser
-            </a>
+            <div className={`d-none d-md-block`}>
+              <a
+                className={`btn btn-link mr-2`}
+                href="/account/#/configuration/browser"
+              >
+                Setup Your Browser
+              </a>
+            </div>
           </If>
 
           <If test={canConfigure}>
-            <a className={`btn ${secondaryVariantButtonClassName}`} href="/account/#/configuration">
+            <a
+              className={`btn ${secondaryVariantButtonClassName}`}
+              href="/account/#/configuration"
+            >
               Configure <Icon icon="settings" />
             </a>
           </If>
         </div>
-        <script type="text/javascript" src="/assets/js/navbar.js"></script>
+        <script type="text/javascript" src="/assets/js/navbar.js" />
       </nav>
     );
   }
