a99a108375e364d8191232717e4ca4a6949ccaa2
Caleb Peterson <caleb.peterson@cubicle6.com>
2018-10-20 19:31:29 -0400

    remove needless logging

--- src/config/merge.js
+++ src/config/merge.js
@@ -1,7 +1,6 @@
 const { merge } = require("lodash");
 
 function mergeConfigurations(userConfig, ...otherConfigs) {
-  console.log();
   return merge({ bangs: {} }, ...otherConfigs, userConfig);
 }
 
