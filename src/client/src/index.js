3f36cd6a2e7a14ec718b7eff9b611a55f0481b6a
Caleb Peterson <caleb.peterson@cubicle6.com>
2019-02-15 08:06:23 -0500

    enable Sentry client-side error capture

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
+if (process.env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({ dsn: process.env.REACT_APP_SENTRY_DSN });
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
